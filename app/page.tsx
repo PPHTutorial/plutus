5c6a11b8b71913fece08680fc794833839f4452c
@@123@Beatbacklist <dev1samtechstudiolab@gmail.com>
2025-12-27 07:50:25 +0000

    GoLive

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ import { redirect } from 'next/navigation'
 
 
 export default async function Home() {
-  redirect("/maintenance")
+  //redirect("/maintenance")
 
   
   const headersList = headers()
